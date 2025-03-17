0239232dc01f09049a21687214367a8d4530386a
Birkity <birkity.yishak.m@gmail.com>
2025-03-17 09:36:06 +0300

    Add childern to action tool bar

--- tik_tok_ui/lib/home.dart
+++ tik_tok_ui/lib/home.dart
@@ -20,6 +20,17 @@ class Home extends StatelessWidget{
   Widget get actionToolbar => Container(
                 width: 100.0,
                 color: Colors.red[300],
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children:
+                    List<Widget>.generate(5, (_) => Container(
+                      width: 60,
+                      height: 60,
+                      color: Colors.blue[300],
+                      margin: EdgeInsets.only(top: 10),
+
+                    )),
+                ),
               );
   Widget get middleSection => Expanded(
     child: Row(
