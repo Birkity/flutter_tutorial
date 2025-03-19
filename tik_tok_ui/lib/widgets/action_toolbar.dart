9479ec71b6b613b17abe20696ac3d223c5b16da2
Birkity <birkity.yishak.m@gmail.com>
2025-03-19 09:05:50 +0300

    Organize constant fields and add _getfollow action function

--- tik_tok_ui/lib/widgets/action_toolbar.dart
+++ tik_tok_ui/lib/widgets/action_toolbar.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-
 class ActionToolbar extends StatelessWidget{
+
+// Full dimensions of an action
+static const double ActionWidgetSize = 60.0;
+
+// The size of the icon showen for Social Actions
+static const double ActionIconSize = 35.0;
+
+// The size of the share social icon
+static const double ShareActionIconSize = 25.0;
+
+// The size of the profile image in the follow Action
+static const double ProfileImageSize = 50.0;
+
+// The size of the plus icon under the profile image in follow action
+static const double PlusIconSize = 20.0;
+
+
   @override
   Widget build(BuildContext context){
     return Container(
@@ -21,15 +37,17 @@ class ActionToolbar extends StatelessWidget{
 
   Widget _getSocialAction({
     required String title,
-    required IconData icon
+    required IconData icon,
+    bool isShare = false
+    
   }){
     return Container(
       margin: EdgeInsets.only(top: 15.0),
-      width: 60.0,
-      height: 60.0,
+      width: ActionWidgetSize,
+      height: ActionWidgetSize,
       child: Column(
         children: [
-          Icon(icon, size: 25.0, color: Colors.grey[300],),
+          Icon(icon, size: isShare ? 25.0 : ActionIconSize, color: Colors.grey[300],),
           Padding(
             padding: EdgeInsets.only(top: 2.0), 
             child: 
@@ -46,8 +64,8 @@ class ActionToolbar extends StatelessWidget{
   }){
     return Container(
       margin: EdgeInsets.symmetric(vertical: 10.0),
-      width: 60.0,
-      height: 60.0,
+      width: ActionWidgetSize,
+      height: ActionWidgetSize,
       child: Stack( children: [
         _getProfilePicture(),
         _getPlusIcon()
