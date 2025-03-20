1eb26f0b46f120c99864872abc47ad592ab98cd3
Birkity <birkity.yishak.m@gmail.com>
2025-03-20 10:19:12 +0300

    feat: add top section with Following and For You text layout

--- tik_tok_ui/lib/widgets/top_section.dart
+++ tik_tok_ui/lib/widgets/top_section.dart
@@ -4,9 +4,18 @@ class TopSection extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Container(
-            height: 100.0,
-            color: Colors.yellow[300],
-            );
+      height: 100.0,
+      padding: EdgeInsets.only(bottom: 15.0),
+      alignment: Alignment(0.0, 1.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+        Text('Following', style: TextStyle(color: Colors.white),),
+        Container(width: 15.0,),
+        Text('For You' ,style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17.0, color: Colors.white),)
+      ],),
+    );
    
   }
 }
