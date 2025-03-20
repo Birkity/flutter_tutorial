466f1b3ed9cd05418290b72ad1a38fc017364a69
Birkity <birkity.yishak.m@gmail.com>
2025-03-20 10:27:02 +0300

    feat: add bottom toolbar with TikTok-style navigation icons

--- tik_tok_ui/lib/widgets/bottom_toolbar.dart
+++ tik_tok_ui/lib/widgets/bottom_toolbar.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 
 class BottomToolbar extends StatelessWidget{
+  static const double NavigationIconSize = 20.0;
+
   @override
   Widget build(BuildContext context) {
     return Row(
     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: List<Widget>.generate(5, (index) => Container(
-      width: 40.0,
-      height: 40.0,
-      color: Colors.purple[300],
-    )),
+    children: [
+      Icon(Icons.home, color: Colors.white, size: NavigationIconSize),
+      Icon(Icons.search,
+                    color: Colors.white, size: NavigationIconSize),
+                
+      Icon(Icons.message,
+                    color: Colors.white, size: NavigationIconSize),
+      Icon(Icons.person,
+                    color: Colors.white, size: NavigationIconSize)
+    ],
   );
   }
 }
