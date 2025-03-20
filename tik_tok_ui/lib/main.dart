6e7a093684b0eff652cc6eee36701ed661e2466b
Birkity <birkity.yishak.m@gmail.com>
2025-03-20 10:38:13 +0300

    feat: add custom TikTok-style create button with layered design

--- tik_tok_ui/lib/main.dart
+++ tik_tok_ui/lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
+      debugShowCheckedModeBanner: false, 
       title: 'TikTok UI',
       theme: ThemeData(
         textTheme: Theme.of(context).textTheme.apply(
